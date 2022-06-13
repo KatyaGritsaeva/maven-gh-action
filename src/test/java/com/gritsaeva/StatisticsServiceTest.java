021a3219c5d7f3ef39a6e12fd6aa39e375b930c5
KatyaGritsaeva <gekaterina2019@gmail.com>
2022-06-13 16:55:08 +0300

    Add test on 100percent

--- src/test/java/com/gritsaeva/StatisticsServiceTest.java
+++ src/test/java/com/gritsaeva/StatisticsServiceTest.java
@@ -16,5 +16,16 @@ import static org.junit.jupiter.api.Assertions .*;
 
             assertEquals(expected, actual);
         }
+        @Test
+        void findMaxInTheMiddle() {
+            StatisticsService service = new StatisticsService();
+
+            long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 25, 8, 6, 11, 11, 12};
+            long expected = 25;
+
+            long actual = service.findMax(incomesInBillions);
+
+            assertEquals(expected, actual);
+        }
     }
 
